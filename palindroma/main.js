f6374a38d27184fbe4015f14c75f02b8bfe2a34f
Gio <giovanni.berl1@gmail.com>
2020-04-24 18:50:31 +0200

    first commit

---
+++ palindroma/main.js
@@ -0,0 +1,26 @@
+// scrivere una funzione per capire se una parola è palindroma. Quindi chiedere una parola all'utente, utilizzare la funzione per sapere se la parola inserita è palindroma e stampare un messaggio appropriato
+
+//chiedo parola all'utente
+var parola = prompt(`inserisci una parola per vedere se e' palindroma`);
+
+// utilizzo la funzione isPalidroma (parametrizzando la parola dell'utente) per sapere se la parola inserita è palindroma e stampo un messaggio appropriato
+if (isPalindroma(parola) == true) {
+    console.log(`la parola ${parola} e' palindroma`);
+} else {
+    console.log(`la parola "${parola}" non e' palindroma`);
+}
+
+function isPalindroma(stringa) {
+    var arrayStringa = stringa.split("");
+    // hello --->  [h,e,l,l,o]
+    var arrayReversed = arrayStringa.reverse();
+    // [h,e,l,l,o] ---> [o,l,l,e,h]
+    var stringaReversed = arrayReversed.join("");
+    // [o,l,l,e,h] ---> olleh
+
+    if (stringa === stringaReversed) {
+        return true;
+    } else {
+        return false;
+    }
+}
---
+++ pari-dispari/main.js
@@ -0,0 +1,35 @@
+// chiedere all'utente un numero tra 1 e 5 e generare un numero random tra 1 e 5. Chiedere all'utente se sceglie pari o dispari, quindi fare la somma dei due numeri (giocata utente + giocata pc), quindi valutare se il risultato è pari o dispari e comunicare all'utente se ha vinto o meno
+
+// chiedere all'utente un numero tra 1 e 5
+var numero = parseInt(prompt("inserisci un numero tra 1 e 5"));
+
+// generare un numero random tra 1 e 5
+var numeroRandom = Math.floor(Math.random() * 5) + 1;
+
+// Chiedere all'utente se sceglie pari o dispari
+var scelta = prompt("pari o dispari?");
+
+// fare la somma dei due numeri (e stamparla)
+var somma = numero + numeroRandom;
+
+// voglio vedere il numero dell'utente e quello del computer, la loro somma e la scelta dell'utente
+console.log(`numero dell' utente: ${numero}`);
+console.log(`numero generato dal computer: ${numeroRandom}`);
+console.log(`la somma dei due numeri e': ${somma}`);
+console.log(`l'utente pensa che la somma sara': ${scelta}`);
+
+// valutare se il risultato è pari o dispari e comunicare all'utente se ha vinto o meno
+if (isPari(somma) == scelta) {
+    console.log("hai vinto");
+} else {
+    console.log("mi dispiace hai perso");
+}
+
+// definisco una funzione per stabilire numeri pari e dispari
+function isPari(num) {
+    if (num % 2 == 0) {
+        return "pari";
+    } else {
+        return "dispari";
+    }
+}
